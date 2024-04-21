e286bd8e6ddc243ad2d21ab44653d85ba7078184
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2024-04-20 21:00:56 -0300

    Registro funcional

--- lib/screens/Login-Screen/login_screen.dart
+++ lib/screens/Login-Screen/login_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_recipes_app/services/auth_service.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
+
+  final AuthService service = AuthService();
 
   @override
   Widget build(BuildContext context) {
@@ -23,17 +26,36 @@ class LoginScreen extends StatelessWidget {
                     "assets/images/logoMyRecipes.jpg",
                     fit: BoxFit.fill,
                   ),
-                  const Text("Login or Register", style: TextStyle(fontWeight: FontWeight.bold,color: Color.fromRGBO(255, 250, 221, 1))),
+                  const Text("Login or Register",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(255, 250, 221, 1))),
                   TextFormField(
                     controller: _emailController,
                     decoration: const InputDecoration(
-                      label: Text("E-mail",style: TextStyle(fontWeight: FontWeight.bold,color: Color.fromRGBO(255, 250, 221, 1))),
+                      label: Text("E-mail",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(255, 250, 221, 1))),
+                    ),
+                    style: const TextStyle(
+                      color: Color.fromRGBO(255, 250, 221, 1), // A cor do texto inserido
                     ),
                     keyboardType: TextInputType.emailAddress,
                   ),
                   TextFormField(
                     controller: _passController,
-                    decoration: const InputDecoration(label: Text("Password", style: TextStyle(fontWeight: FontWeight.bold,color: Color.fromRGBO(255, 250, 221, 1)))),
+                    decoration: const InputDecoration(
+                      label: Text(
+                        "Password",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(255, 250, 221, 1)),
+                      ),
+                    ),
+                    style: const TextStyle(
+                      color: Color.fromRGBO(255, 250, 221, 1), // A cor do texto inserido
+                    ),
                     keyboardType: TextInputType.visiblePassword,
                     maxLength: 16,
                     obscureText: true,
@@ -41,12 +63,18 @@ class LoginScreen extends StatelessWidget {
                   ElevatedButton(
                     onPressed: () {
                       //TO-DO
+                      //register(context);
                       //login(context);
                     },
                     style: ElevatedButton.styleFrom(
-                      foregroundColor: const Color(0xFFFFFADD), backgroundColor: const Color(0xFFDC6425),  // RGB(255,250,221)
+                      foregroundColor: const Color(0xFFFFFADD),
+                      backgroundColor:
+                          const Color(0xFFDC6425), // RGB(255,250,221)
                     ),
-                    child: const Text("Let's cook!", style: TextStyle(fontWeight: FontWeight.bold,color: Color.fromRGBO(255, 250, 221, 1)) ),
+                    child: const Text("Let's cook!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(255, 250, 221, 1))),
                   ),
                 ],
               ),
@@ -55,5 +83,16 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  register(BuildContext context) async {
+    String email = _emailController.text;
+    String password = _passController.text;
+
+    service.register(email: email, password: password).then((response) {
+      if (response) {
+        Navigator.pushReplacementNamed(context, "home");
+      }
+    });
   }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:my_recipes_app/services/web_client.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthService{
+class AuthService {
   String url = WebClient.url; //atributo estático, não precisa de instancia
-  http.Client client = WebClient().client; //client com http_interceptors e timeout definido
+  http.Client client =
+      WebClient().client; //client com http_interceptors e timeout definido
 
   //TO-DO: login register
 
+  Future<bool> register(
+      {required String email, required String password}) async {
+    final Map<String, String> requestBody = {
+      'email': email,
+      'password': password,
+    };
+    http.Response response = await client.post(
+      Uri.parse('${url}register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(requestBody),
+    );
+    if(response.statusCode !=201){
+      throw HttpException(response.body);
+    }
+    return true;
+  }
 }
